4875c34aeeba8485e3906d91cb5fcee42220058c
Allan Oricil <allanoricilcos@outlook.com>
2024-07-24 10:38:04 -0300

    chore: removing NOTE which is no longer necessary

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { registerNodes } from "src/lib/helper";
 import { Node1, Node2 } from "@nodes";
 
-// NOTE: this could created at build time if there was a way to map node name with class name from within the node class itself
 export default function (RED) {
   registerNodes(RED, [Node1, Node2]);
 }
